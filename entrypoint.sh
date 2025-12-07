14d1476a264ba23d63f135d2c2b79b6bdf2e6dec
Rileythedreamer <crazynegin@gmail.com>
2025-12-07 12:21:48 +0330

    modified deployment workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,26 @@
+# #!/bin/bash
+# set -e
+
+# # Wait for DB
+# ./wait-for-db.sh db 5432
+
+# # Apply migrations
+# python manage.py migrate --noinput
+
+# # Load initial data 
+# if [ ! -f "/app/.data_loaded" ]; then
+#     python manage.py loaddata initial_data.json || true
+#     touch /app/.data_loaded
+# fi
+
+
+# # exec "$@"
+# exec gunicorn EventHub.wsgi:application \
+#     --bind 0.0.0.0:8000 \
+#     --workers 3 \
+#     --log-level info
+
+
 #!/bin/bash
 set -e
 
@@ -7,14 +30,16 @@ set -e
 # Apply migrations
 python manage.py migrate --noinput
 
+# Collect static files (so CSS/JS changes reflect)
+python manage.py collectstatic --noinput
+
 # Load initial data 
 if [ ! -f "/app/.data_loaded" ]; then
     python manage.py loaddata initial_data.json || true
     touch /app/.data_loaded
 fi
 
-
-# exec "$@"
+# Start Gunicorn
 exec gunicorn EventHub.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3 \
