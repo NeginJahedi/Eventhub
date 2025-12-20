52a613eee712477e1ba19d07d198a8333abde2f4
Rileythedreamer <crazynegin@gmail.com>
2025-12-20 17:50:22 +0330

    tests updated

--- events/tests/test_concurrency.py
+++ events/tests/test_concurrency.py
@@ -2,27 +2,28 @@ import pytest
 import threading
 from django.urls import reverse
 from django.test import Client
-
+from events.models import Ticket
 from events.tests.factories import EventFactory, UserFactory
 
 
 @pytest.mark.django_db(transaction=True)
 def test_no_oversell_under_concurrency():
     event = EventFactory(tickets_available=1)
-    users = [UserFactory(password="password123") for _ in range(2)]
+    users = [UserFactory() for _ in range(2)]
+    for user in users:
+        user.set_password("password123")
+        user.save()
 
     def attempt_purchase(user):
         client = Client()
         client.login(username=user.username, password="password123")
         client.post(
-            reverse("buy", args=[event.id]),
+            reverse("buy_ticket", args=[event.id]),
             {"quantity": 1},
+            follow=True,
         )
 
-    threads = [
-        threading.Thread(target=attempt_purchase, args=(user,))
-        for user in users
-    ]
+    threads = [threading.Thread(target=attempt_purchase, args=(user,)) for user in users]
 
     for t in threads:
         t.start()
--- events/tests/test_ticket_purchse.py
+++ events/tests/test_ticket_purchse.py
@@ -2,20 +2,27 @@ import pytest
 from django.urls import reverse
 
 from events.tests.factories import EventFactory, UserFactory
-
+from events.models import Ticket
 
 @pytest.mark.django_db
 def test_buy_ticket_success(client):
     user = UserFactory(is_attendee=True)
+    user.save()
     event = EventFactory(tickets_available=5)
-
-    client.login(username=user.username, password="password123")
+    event.save()
+    
+    client.force_login(username=user.username, password="password123")
 
     response = client.post(
         reverse("buy", args=[event.id]),
         {"quantity": 2},
         follow=True,
     )
+    print("status", response.status_code)
+    print("content:", response.content.decode()[:500])
+    
+    print("tickets count:", Ticket.objects.filter(event=event).count())
+
 
     event.refresh_from_db()
 
