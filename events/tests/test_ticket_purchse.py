f1c4bceac2a17757b32c2d33ecbebc09c5b2172d
Rileythedreamer <crazynegin@gmail.com>
2025-12-20 16:27:21 +0330

    debugging tests

--- events/tests/test_ticket_purchse.py
+++ events/tests/test_ticket_purchse.py
@@ -1,17 +1,23 @@
 from django.urls import reverse
 import pytest
+
 from events.tests.factories import EventFactory, UserFactory
 
 
 @pytest.mark.django_db
 def test_buy_ticket_success(client):
-    user = UserFactory()
-    event = EventFactory(total_tickets=5)
+    user = UserFactory(is_attendee=True)
+    event = EventFactory(tickets_available=5)
 
     client.login(username=user.username, password="password123")
 
-    response = client.post(reverse("buy_ticket", args=[event.id]), {"quantity": 2})
+    response = client.post(
+        reverse("buy_ticket", args=[event.id]),
+        {"quantity": 2},
+    )
 
     event.refresh_from_db()
+
     assert response.status_code == 200
+    assert event.tickets_sold() == 2
     assert event.tickets_remaining() == 3
