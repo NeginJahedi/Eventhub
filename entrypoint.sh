66a8b67e7043be529a3e83c8a311e33428c88141
Rileythedreamer <crazynegin@gmail.com>
2025-12-21 17:28:25 +0330

    tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,47 +1,25 @@
-# #!/bin/bash
-# set -e
-
-# # Wait for DB
-# /wait-for-db.sh db 5432
-
-# # Apply migrations
-# python manage.py migrate --noinput
-
-# python manage.py collectstatic --noinput
-
-# # Load initial data 
-# if [ ! -f "/app/.data_loaded" ]; then
-#     python manage.py loaddata initial_data.json || true
-#     touch /app/.data_loaded
-# fi
-
-
-# # exec "$@"
-# exec gunicorn EventHub.wsgi:application \
-#     --bind 0.0.0.0:8000 \
-#     --workers 3 \
-#     --log-level info
-
-
 #!/bin/bash
 set -e
 
+# Wait for DB
 /wait-for-db.sh db 5432
 
+# Apply migrations
 python manage.py migrate --noinput
+
 python manage.py collectstatic --noinput
 
+# Load initial data 
 if [ ! -f "/app/.data_loaded" ]; then
     python manage.py loaddata initial_data.json || true
     touch /app/.data_loaded
 fi
 
-# If no command is passed, run gunicorn by default
-if [ "$#" -eq 0 ]; then
-    exec gunicorn EventHub.wsgi:application \
-        --bind 0.0.0.0:8000 \
-        --workers 3 \
-        --log-level info
-else
-    exec "$@"
-fi
+
+# exec "$@"
+exec gunicorn EventHub.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --log-level info
+
+
