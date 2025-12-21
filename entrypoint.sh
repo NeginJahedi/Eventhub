f6b86a3927485b4121ab19b62fef3381f5ca9904
Rileythedreamer <crazynegin@gmail.com>
2025-12-21 17:14:17 +0330

    tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,28 @@
+# #!/bin/bash
+# set -e
+
+# # Wait for DB
+# /wait-for-db.sh db 5432
+
+# # Apply migrations
+# python manage.py migrate --noinput
+
+# python manage.py collectstatic --noinput
+
+# # Load initial data 
+# if [ ! -f "/app/.data_loaded" ]; then
+#     python manage.py loaddata initial_data.json || true
+#     touch /app/.data_loaded
+# fi
+
+
+# # exec "$@"
+# exec gunicorn EventHub.wsgi:application \
+#     --bind 0.0.0.0:8000 \
+#     --workers 3 \
+#     --log-level info
+
+
 #!/bin/bash
 set -e
 
@@ -7,19 +32,14 @@ set -e
 # Apply migrations
 python manage.py migrate --noinput
 
+# Collect static files
 python manage.py collectstatic --noinput
 
-# Load initial data 
+# Load initial data
 if [ ! -f "/app/.data_loaded" ]; then
     python manage.py loaddata initial_data.json || true
     touch /app/.data_loaded
 fi
 
-
-# exec "$@"
-exec gunicorn EventHub.wsgi:application \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --log-level info
-
-
+# Run the container command (runserver, pytest, gunicorn, etc.)
+exec "$@"
